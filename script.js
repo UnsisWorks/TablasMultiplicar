34f589f7864017b7e1b3d10203cb80f4a4d70e2c
Tu Nombre <you@example.com>
2023-06-05 16:54:33 -0600

    ededed

--- script.js
+++ script.js
@@ -11,24 +11,26 @@ function crear() {
 }
 
 function createTable(init, end) {
-     document.write("<table>");
-     document.write("<tbody>");
-
-
-     document.write("<tr> <th>Tabla</th> <th>Mult</th> <th>Rs</th> </tr>");
-
-     for (let i = init; i < end; i++) {
-          const element = array[i];
-          document.write("<tr>");
-          document.write("<td> fref </td>");
-          document.write("</tr>");
-          document.write("<td>");
-          document.write("</tr>");
-          
+     
+     
+     for (let i = init; i <= end; i++) {
+          document.write("<table>");
+          document.write("<tbody>");
+          document.write("<tr> <th>Tabla</th> <th>Mult</th> <th>Rs</th> </tr>");
+          for (let j = 1; j <= 10; j++) {
+               console.log("entro")
+               document.write("<tr>");
+               
+               document.write("<td> " + i + " </td>");
+               document.write("<td> " + j + " </td>");
+               document.write("<td> " + i*j + " </td>");
+               
+               document.write("</tr>");
+          }
+          document.write("</tbody>");
+          document.write("</table>");
      }
 
-     document.write("</tbody>");
-     document.write("</table>");
 }
 
 run();
